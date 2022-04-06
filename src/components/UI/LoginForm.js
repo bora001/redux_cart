16358d44da6b0d657e6486a229e19510c66a4a53
unknown <bora.vancouver@gmail.com>
2022-04-06 11:47:12 +0900

    login/register form

--- src/components/UI/LoginForm.js
+++ src/components/UI/LoginForm.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Modal from "./Modal";
+import "./LoginForm.css";
 
 const LoginForm = (props) => {
   return (
     <Modal>
-      LoginForm;
-      <button onClick={props.modalClose}>Close</button>
+      <form className="form_login">
+        <h2>Login</h2>
+        <div className="input_box">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div className="input_box">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <div className="btn_box">
+          <button>Login</button>
+          <button onClick={props.modalClose}>Close</button>
+        </div>
+      </form>
     </Modal>
   );
 };
--- src/components/UI/RegisterForm.js
+++ src/components/UI/RegisterForm.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Modal from "./Modal";
-
+import "./RegisterForm.css";
 const RegisterForm = (props) => {
   return (
     <Modal>
-      RegisterForm
-      <button onClick={props.modalClose}>Close</button>
+      <form className="form_register">
+        <h2>Register</h2>
+        <div className="input_box">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div className="input_box">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <div className="input_box">
+          <label htmlFor="pwdCheck">Password Check</label>
+          <input type="password" id="pwdCheck" name="pwdCheck" />
+        </div>
+        <div className="btn_box">
+          <button>Register</button>
+          <button onClick={props.modalClose}>Close</button>
+        </div>
+      </form>
     </Modal>
   );
 };

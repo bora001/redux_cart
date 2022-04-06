2e7cf059b3e0b3d3c4c203e65cdd4b0d431bce6d
unknown <bora.vancouver@gmail.com>
2022-04-06 15:06:32 +0900

    register errMsg

--- src/components/UI/RegisterForm.js
+++ src/components/UI/RegisterForm.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Modal from "./Modal";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase-config";
 import "./RegisterForm.css";
-import { firebaseUrl } from "../../dev";
 
 const RegisterForm = (props) => {
+  const [errMsg, setErrMsg] = useState("");
+  const inputBox = document.querySelectorAll(".input_box input");
+  const ref = useRef();
   let data = {};
+
+  const typing = () => {
+    setErrMsg("");
+  };
+
   const submitForm = (e) => {
     e.preventDefault();
-    const inputBox = document.querySelectorAll(".input_box input");
 
     inputBox.forEach((input) => {
       data[input.id] = input.value;
+      input.value = "";
     });
+
+    if (data.password !== data.pwdCheck) {
+      setErrMsg("Wrong password");
+      return;
+    }
 
     postData(data);
   };
@@ -30,30 +42,41 @@ const RegisterForm = (props) => {
       }
       console.log(user, "user");
     } catch (err) {
-      console.log(err.message);
+      setErrMsg(err.message.split(":"));
     }
   };
 
   return (
     <Modal>
-      <form className="form_register" onSubmit={submitForm}>
+      <form className="form_register" onSubmit={submitForm} ref={ref}>
         <h2>Register</h2>
         <div className="input_box">
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" />
+          <input type="text" id="email" name="email" onChange={typing} />
         </div>
         <div className="input_box">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={typing}
+          />
         </div>
         <div className="input_box">
           <label htmlFor="pwdCheck">Password Check</label>
-          <input type="password" id="pwdCheck" name="pwdCheck" />
+          <input
+            type="password"
+            id="pwdCheck"
+            name="pwdCheck"
+            onChange={typing}
+          />
         </div>
         <div className="btn_box">
           <button>Register</button>
           <button onClick={props.modalClose}>Close</button>
         </div>
+        {errMsg && <p className="err_txt">{errMsg}</p>}
       </form>
     </Modal>
   );

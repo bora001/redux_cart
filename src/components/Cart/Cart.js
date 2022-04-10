4e7d0fd5bbfa436d411b4001ab29e36eeba5a204
unknown <bora.vancouver@gmail.com>
2022-04-10 16:21:45 +0900

    css:cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,21 +2,34 @@ import React, { useState, useEffect } from "react";
 import Modal from "../UI/Modal";
 import { useSelector, useDispatch } from "react-redux";
 import CartItem from "./CartItem";
+import "./Cart.css";
 
 const Cart = (props) => {
   const state = useSelector((state) => state.cartReducer);
 
   return (
     <Modal>
-      <h3>Cart Items</h3>
-      {state.items.map((item) => (
-        <CartItem item={item} key={item.name} />
-      ))}
-      <div className="total_box">
-        <p>$ {state.total}</p>
+      <div className="cart_box">
+        <h3>My Cart</h3>
+
+        {state.items.length ? (
+          <>
+            {state.items.map((item) => (
+              <CartItem item={item} key={item.name} />
+            ))}
+            <div className="total_box">
+              <p>Total</p>
+              <p>$ {state.total}</p>
+            </div>
+          </>
+        ) : (
+          <p>Your cart is empty</p>
+        )}
+        <div className="btn_box">
+          {state.items.length > 0 && <button>Checkout</button>}
+          <button onClick={props.modalClose}>Close</button>
+        </div>
       </div>
-      <button>Checkout</button>
-      <button onClick={props.modalClose}>Close</button>
     </Modal>
   );
 };

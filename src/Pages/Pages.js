50c726711dc63f7472cbec02a0ebdf443ea85a18
unknown <bora.vancouver@gmail.com>
2022-04-20 14:33:14 +0900

    filter by price

--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -25,28 +25,42 @@ const Pages = (props) => {
       .get(firebaseKey.firebaseUrl + `Item/${params.item}.json`)
       .then((data) => setProductList(Object.values(data.data)));
   }, [params.item]);
-  console.log(params);
+
+  const sortPrice = (e) => {
+    let sort = productList.sort((a, b) =>
+      e.target.value == "low" ? a.price - b.price : b.price - a.price
+    );
+    setProductList((prevList) => [...sort]);
+  };
 
   return (
     <div className="page_box">
       {!productList && <p>{params.item} Pages</p>}
-      {productList &&
-        productList.map((item) => (
-          <div className="items">
-            <div className="img_box">
-              <img src={item.img} alt={item.name} />
-            </div>
-            <div className="txt_box">
-              <div className="item_desc">
-                <h3>{item.name}</h3>
-                <p>$ {item.price}</p>
+      <div className="filter_box">
+        <select onChange={sortPrice}>
+          <option value="high">High price</option>
+          <option value="low">Low price</option>
+        </select>
+      </div>
+      <div className="item_box">
+        {productList &&
+          productList.map((item) => (
+            <div className="items">
+              <div className="img_box">
+                <img src={item.img} alt={item.name} />
               </div>
-              <button className="btn_add" onClick={() => addCart(item)}>
-                Add to Cart
-              </button>
+              <div className="txt_box">
+                <div className="item_desc">
+                  <h3>{item.name}</h3>
+                  <p>$ {item.price}</p>
+                </div>
+                <button className="btn_add" onClick={() => addCart(item)}>
+                  Add to Cart
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+      </div>
     </div>
   );
 };
